e903d92b3395da594fc84319bb2667a51d8f1736
Oleg Skutte <00.00.oleg.00.00@gmail.com>
2024-10-09 23:08:57 +0400

    Use GNU

--- build.rs
+++ build.rs
@@ -1,5 +1,16 @@
+use std::process::Command;
+use std::env;
+
 fn main() {
     if cfg!(target_os = "windows") {
-        println!("cargo:rustc-link-arg=.\\DreamioUpdater.res");
+        let out_dir = env::var("OUT_DIR").unwrap();
+        let status = Command::new("windres")
+            .args(&["src/DreamioUpdater.rc", "-o"])
+            .arg(&format!("{}/DreamioUpdater.o", out_dir))
+            .status()
+            .unwrap();
+        assert!(status.success(), "Failed to run windres");
+        println!("cargo:rustc-link-search=native={}", out_dir);
+        println!("cargo:rustc-link-arg={}/DreamioUpdater.o", out_dir);
     }
 }
